315784ee39b02b88f2e8d20e68f3c26764943f47
nicholasham <nicholas.hammond@form3.tech>
2020-12-10 21:21:28 +0000

    refactored collector

--- pkg/sink/collector.go
+++ pkg/sink/collector.go
@@ -3,6 +3,8 @@ package sink
 import (
 	"context"
 
+	"github.com/form3tech-oss/go-flow/pkg/option"
+
 	"github.com/form3tech-oss/go-flow/pkg/stream"
 )
 
@@ -17,23 +19,35 @@ func (s *collectorSink) Input() chan stream.Element {
 }
 
 type Collector interface {
-	SetInput(input chan stream.Element)
-	Run(ctx context.Context)
+	Collect(element stream.Element)
 }
 
 func (s *collectorSink) WireSourceToSink(source stream.Source) stream.Runnable {
 	s.source = source
-	s.collector.SetInput(s.input)
 	return s
 }
 
 func (s *collectorSink) Run(ctx context.Context) {
 	s.source.Run(ctx)
-	s.collector.Run(ctx)
+	go func() {
+		for s.input != nil {
+			select {
+			case <-ctx.Done():
+				return
+			case element, ok := <-s.input:
+				if !ok {
+					return
+				}
+				s.collector.Collect(element)
+			default:
+			}
+		}
+	}()
 }
 
-func FromCollector(collector Collector) stream.Sink {
+func FromCollector(collector Collector, options ...option.Option) stream.Sink {
 	return &collectorSink{
 		collector: collector,
+		input:     option.CreateChannel(options...),
 	}
 }
--- pkg/sink/console.go
+++ pkg/sink/console.go
@@ -1,39 +1,19 @@
 package sink
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/form3tech-oss/go-flow/pkg/option"
-
 	"github.com/form3tech-oss/go-flow/pkg/stream"
 )
 
 type consoleCollector struct {
-	input chan stream.Element
 }
 
-func (c *consoleCollector) SetInput(input chan stream.Element) {
-	c.input = input
-}
-
-func (c *consoleCollector) Run(ctx context.Context) {
-	go func() {
-		for c.input != nil {
-			select {
-			case <-ctx.Done():
-				return
-			case element, ok := <-c.input:
-				if !ok {
-					return
-				}
-				fmt.Println(element)
-			default:
-			}
-		}
-	}()
+func (c *consoleCollector) Collect(element stream.Element) {
+	fmt.Println(element)
 }
 
 func Console(options ...option.Option) stream.Sink {
-	return FromCollector(&consoleCollector{})
+	return FromCollector(&consoleCollector{}, options...)
 }
